4fc09aa5d9e69e0d0e310c4d167effe4ae511ecb
PC-DKK\Daniel <danielkomesu@gmail.com>
2018-06-26 20:56:33 -0300

    Add commons module

---
+++ commons.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import os
+import zipfile
+
+
+def open_ncm_file(filename):
+    d = pd.read_csv(
+        filename,
+        sep=";",
+        decimal=",",
+        header=0,
+        encoding="latin1",
+        dtype=str
+    )
+
+    return d
+
+
+def open_mdic_file(filename):
+    d = pd.read_csv(
+        filename, sep=";", decimal=",", header=0, encoding="latin-1",
+        dtype={
+            "CO_NCM": str,
+            "CO_UNID": str,
+            "CO_SH6": str,
+            "CO_PAIS": str,
+            "CO_UF": str,
+            "CO_PORTO": str,
+            "CO_VIA": str,
+            "CO_ANO": str,
+            "CO_MES": str,
+            "QT_ESTAT": np.float64,
+            "KG_LIQUIDO": np.float64,
+            "VL_FOB": np.float64,
+        }
+    )
+
+    d = d.sort_values(by=["CO_NCM", "CO_MES", "CO_ANO"], ascending=True)
+
+    return d
+
+
+def open_zip_mdic_data(filename):
+    print("Abrindo arquivo:", filename)
+    with zipfile.ZipFile(filename) as zf:
+        with zf.open(zf.namelist()[0]) as zfc:
+            d = pd.read_csv(
+                zfc, sep=";", decimal=",", header=0, encoding="latin1",
+                dtype={
+                    "CO_NCM": str,
+                    "CO_UNID": str,
+                    "CO_SH6": str,
+                    "CO_PAIS": str,
+                    "CO_UF": str,
+                    "CO_PORTO": str,
+                    "CO_VIA": str,
+                    "CO_ANO": str,
+                    "CO_MES": str,
+                    "QT_ESTAT": np.float64,
+                    "KG_LIQUIDO": np.float64,
+                    "VL_FOB": np.float64,
+                }
+            )
+
+    return d
+
+
+def select(df, column, value):
+    if type(value) is list:
+        df = df.loc[df[column].isin(value)]
+    else:
+        df = df.loc[df[column] == value]
+
+    return df
