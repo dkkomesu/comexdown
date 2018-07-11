cf1b6ce96178ba493250f7eb0d151f5bd5526630
Daniel Komesu <daniel.komesu@iea.sp.gov.br>
2018-07-11 09:48:35 -0300

    Change to separate by prefix ncm code
    --HG--
    branch : develop

--- src/ts_by_ncm.py
+++ src/ts_by_ncm.py
@@ -6,7 +6,7 @@ import commons
 PATH_DATA = os.path.join("..", "DATA")
 PATH_DATA_EXP = os.path.join(PATH_DATA, "exp")
 PATH_DATA_IMP = os.path.join(PATH_DATA, "imp")
-PATH_NCM_FILE = os.path.join(PATH_DATA, "ncm", "NCM.csv")
+PATH_NCM_FILE = os.path.join(PATH_DATA, "ncm", "ncm_names.csv")
 PATH_PLOTS = os.path.join("..", "plots")
 PATH_PLOTS_EXP = os.path.join(PATH_PLOTS, "ncm_exp")
 PATH_PLOTS_IMP = os.path.join(PATH_PLOTS, "ncm_imp")
@@ -28,17 +28,15 @@ def ncm_ts(directory, ncm_cod):
              if os.path.isfile(os.path.join(directory, f))]
     df = pd.DataFrame()
     for f in files:
-        print("File", f, "NCM", ncm_cod)
         d = commons.open_mdic_file(f)
-        l0 = len(d)
-        d = d.loc[d["CO_NCM"] == ncm_cod, :]
+        d = d.loc[d["CO_NCM"].isin(ncm_cod), :]
         d = d.rename(columns={"CO_ANO": "year", "CO_MES": "month"})
         d = d.assign(day=1)
-        d.loc[:, "date"] = pd.to_datetime(d[["year", "month", "day"]])
+        d.loc[:, "Date"] = pd.to_datetime(d[["year", "month", "day"]])
         d = d.drop(labels=["year", "month", "day"], axis=1)
-        d = d.set_index("date")
+        d = d.set_index("Date")
         l1 = len(d)
-        print("  len", l0, l1)
+        print(f"    File {f}, Lines: {l1}")
         df = pd.concat([df, d], axis=0)
 
     return df
@@ -48,16 +46,19 @@ PATH_DATA_EXP_BY_NCM = os.path.join(PATH_DATA_EXP, "by_ncm")
 if not os.path.exists(PATH_DATA_EXP_BY_NCM):
     os.makedirs(PATH_DATA_EXP_BY_NCM)
 
-for ncm_cod in ncm_data["CO_NCM"].unique():
-    print(ncm_cod)
-    d = ncm_ts(PATH_DATA_EXP, ncm_cod)
-    print("  LEN", len(d))
-    d.to_csv(
-        os.path.join(PATH_DATA_EXP_BY_NCM, f"{ncm_cod}.csv"),
-        sep=";",
-        decimal=",",
-        encoding="latin-1",
-    )
+for n0 in ncm_data["N0"].unique():
+    c_n0 = ncm_data.loc[ncm_data["N0"] == n0, :]
+    for n1 in c_n0["N1"].unique():
+        print(n0, n1)
+        c_n1 = c_n0.loc[c_n0["N1"] == n1, :]
+        d = ncm_ts(PATH_DATA_EXP, c_n1["CO_NCM"])
+        print("  LEN", len(d))
+        d.to_csv(
+            os.path.join(PATH_DATA_EXP_BY_NCM, f"{n0}{n1}.csv"),
+            sep=";",
+            decimal=",",
+            encoding="latin-1",
+        )
 
 
 PATH_DATA_IMP_BY_NCM = os.path.join(PATH_DATA_EXP, "by_ncm")
