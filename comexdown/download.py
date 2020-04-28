c60916c0aaa86bc5ffd498f0e8f0cffbe0fb0200
Daniel Komesu <danielkomesu@gmail.com>
2020-04-28 16:41:47 -0300

    Add docstrings to functions

--- comexdown/download.py
+++ comexdown/download.py
@@ -58,10 +58,25 @@ CANON_EXP_MUN = CANON_URL_DATA + "mun/EXP_{year}_MUN.csv"
 CANON_IMP_MUN = CANON_URL_DATA + "mun/IMP_{year}_MUN.csv"
 CANON_EXP_NBM = CANON_URL_DATA + "nbm/EXP_{year}_NBM.csv"
 CANON_IMP_NBM = CANON_URL_DATA + "nbm/IMP_{year}_NBM.csv"
-# Fonte: http://www.mdic.gov.br/index.php/comercio-exterior/estatisticas-de-comercio-exterior/base-de-dados-do-comercio-exterior-brasileiro-arquivos-para-download
+# Fonte: 
+# http://www.mdic.gov.br/index.php/comercio-exterior/estatisticas-de-comercio-exterior/base-de-dados-do-comercio-exterior-brasileiro-arquivos-para-download
 
 
 def download(url, path, retry=3, blocksize=1024):
+    """Downloads the file in `url` and saves it in `path`
+
+    Parameters
+    ----------
+    url: str
+        The resource's URL to download
+    path: str
+        The destination path of downloaded file
+    retry: int [default=3]
+        Number of retries until raising exception
+    blocksize: int [default=1024]
+        The block size of requests
+
+    """
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -108,6 +123,16 @@ def download(url, path, retry=3, blocksize=1024):
 
 
 def ncm(table, path):
+    """Downloads a NCM table
+
+    Parameters
+    ----------
+    table: str
+        NCM table name to download
+    path: str
+        Destination path directory to save file
+
+    """
     download(
         CANON_URL_NCM_TABLES.format(NCM_TABLES[table]),
         os.path.join(path, "ncm"),
@@ -115,6 +140,16 @@ def ncm(table, path):
 
 
 def nbm(table, path):
+    """Downloads a NBM file
+
+    Parameters
+    ----------
+    table: str
+        NBM table name to download
+    path: str
+        Destination path directory to save file
+
+    """
     download(
         CANON_URL_NBM_TABLES.format(NBM_TABLES[table]),
         os.path.join(path, "nbm"),
@@ -122,6 +157,16 @@ def nbm(table, path):
 
 
 def code(table, path):
+    """Downloads a code file
+
+    Parameters
+    ----------
+    table: str
+        code table name to download
+    path: str
+        Destination path directory to save file
+
+    """
     download(
         CANON_URL_CODE_TABLES.format(CODE_TABLES[table]),
         os.path.join(path, "code"),
@@ -129,29 +174,90 @@ def code(table, path):
 
 
 def exp(year, path):
+    """Downloads a exp file
+
+    Parameters
+    ----------
+    year: int
+        exp year to download
+    path: str
+        Destination path directory to save file
+
+    """
     url = CANON_EXP.format(year=year)
     download(url, os.path.join(path, "exp"))
 
 
 def imp(year, path):
+    """Downloads a imp file
+
+    Parameters
+    ----------
+    year: int
+        imp year to download
+    path: str
+        Destination path directory to save file
+
+    """
     url = CANON_IMP.format(year=year)
     download(url, os.path.join(path, "imp"))
 
 
 def exp_mun(year, path):
+    """Downloads a exp_mun file
+
+    Parameters
+    ----------
+    year: int
+        exp_mun year to download
+    path: str
+        Destination path directory to save file
+
+    """
     url = CANON_EXP_MUN.format(year=year)
     download(url, os.path.join(path, "exp_mun"))
 
 
 def imp_mun(year, path):
+    """Downloads a imp_mun file
+
+    Parameters
+    ----------
+    year: int
+        imp_mun year to download
+    path: str
+        Destination path directory to save file
+
+    """
     url = CANON_IMP_MUN.format(year=year)
     download(url, os.path.join(path, "imp_mun"))
 
+
 def exp_nbm(year, path):
+    """Downloads a exp_nbm file
+
+    Parameters
+    ----------
+    year: int
+        exp_nbm year to download
+    path: str
+        Destination path directory to save file
+
+    """
     url = CANON_EXP_NBM.format(year=year)
     download(url, os.path.join(path, "exp_nbm"))
 
 
 def imp_nbm(year, path):
+    """Downloads a imp_nbm file
+
+    Parameters
+    ----------
+    year: int
+        imp_nbm year to download
+    path: str
+        Destination path directory to save file
+
+    """
     url = CANON_IMP_NBM.format(year=year)
     download(url, os.path.join(path, "imp_nbm"))
