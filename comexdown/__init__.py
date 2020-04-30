14c6f7338d00c4c872746722ef8fc4f7dda2a11a
Daniel Komesu <danielkomesu@gmail.com>
2020-04-30 12:20:43 -0300

    Make comexdown.download functions avaible at package level

--- comexdown/__init__.py
+++ comexdown/__init__.py
@@ -1,3 +1,8 @@
+from .download import (
+    ncm, nbm, code, exp, imp, exp_mun, imp_mun, exp_nbm, imp_nbm
+)
+
+
 __author__ = "Daniel K. Komesu"
 __author_email__ = "danielkomesu@gmail.com"
 __version__ = "0.8"
