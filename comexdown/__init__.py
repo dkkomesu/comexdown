a6c1187cf85528792221c5754f572dad527f35ad
Daniel Komesu <danielkomesu@gmail.com>
2020-08-31 20:25:13 -0300

    Bump version

--- comexdown/__init__.py
+++ comexdown/__init__.py
@@ -5,7 +5,7 @@ from comexdown import download
 from comexdown import fs
 
 
-__version__ = "1.0"
+__version__ = "1.1"
 
 
 # -----------------------------------DOWNLOAD-----------------------------------
